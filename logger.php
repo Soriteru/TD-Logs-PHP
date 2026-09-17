7d1995b274edabdbefe211a5ff28d2f72bef0787
Soriteru <sohanblemandpro@gmail.com>
2026-09-17 15:35:34 +0200

    Ajout de la fonctionnalité des messages

--- logger.php
+++ logger.php
@@ -1,13 +1,17 @@
 <?php 
 
-function write_log($username = null){
+function write_log($username = null, $message = "Je suis vendeur chez Carrefour"){
     $fp = fopen('app.log', 'a');
     $date = $date = date("Y-m-d H:i:s");
     if(empty($username)){
         $username = "UNKNOWN";
     }
+
+    if(empty($message)) {
+        $message = "EMPTY";
+    }
     
-    fwrite($fp, "[" . $date . "]" . " - " . $username . "\n");
+    fwrite($fp, "[" . $date . "]" . " - " . $username . " - " . "'" . $message . "'" .  "\n");
     fclose($fp);
 }
 
